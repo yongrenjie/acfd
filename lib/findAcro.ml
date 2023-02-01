1eadca169eaf3bbe880bc36c1729044a6b4c3b2f
Jonathan Yong <yongrenjie@gmail.com>
2023-02-01 00:23:19 +0000

    one (1) documentation comment

--- lib/findAcro.ml
+++ lib/findAcro.ml
@@ -1,3 +1,5 @@
+(** [is_acro word min_caps] returns a bool indicating whether there are at
+    least [min_caps] capital letters in [word]. *)
 let is_acro word min_caps =
     let acc_caps acc c = acc + (if Char.lowercase_ascii c <> c then 1 else 0) in
     let n_caps = String.fold_left acc_caps 0 word in
